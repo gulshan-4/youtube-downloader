d786a9a24aad347803468e38484fea9a1ad47edb
Sachin <2704.fgc@gmail.com>
2024-12-09 18:19:09 +0530

    trying to fix vercel frontend eslintrc errror

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import LogoBar from './components/LogoBar'
 import Footer from './components/Footer'
-import StoreProvider from './storeProvider'
+import StoreProvider from './StoreProvider'
 import "./components/component-styles.css";
 
 const inter = Inter({ subsets: ['latin'] })
